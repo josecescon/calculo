7f11572149659a67a699edb8f3cf41fced913389
josecescon <jose.cescon@escola.pr.gov.br>
2025-06-12 12:03:13 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+function soma(){
+    let numero1 = parseInt (document.getElementById('numero1').value);
+    let numero2 = parseInt(document.getElementById('numero2').value);
+    let soma = numero1 + numero2;
+    document.getElementById('resultado').textContent ='Resultado: ' + soma;
+
+}
+function subtrair(){
+    let numero1 = parseInt (document.getElementById('numero1').value);
+    let numero2 = parseInt(document.getElementById('numero2').value);
+    let soma = numero1 - numero2;
+    document.getElementById('resultado2').textContent ='Resultado: ' + soma;
+
+}
