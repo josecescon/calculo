0eb81527fa2b53653958c20473db886032917b0a
josecescon <jose.cescon@escola.pr.gov.br>
2025-06-26 07:50:49 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -12,3 +12,20 @@ function subtrair(){
     document.getElementById('resultado2').textContent ='Resultado: ' + subtrair;
 
 }
+function multiplicar(){
+    let numero1 = parseInt (document.getElementById('numero1').value);
+    let numero2 = parseInt(document.getElementById('numero2').value);
+    let multiplicar = numero1 * numero2;
+      document.getElementById('resultado3').textContent ='Resultado: ' + multiplicar;
+}
+function dividir(){
+    let numero1 = parseFloat (document.getElementById('numero1').value);
+    let numero2 = parseFloat(document.getElementById('numero2').value);
+    if(numero2 == 0){
+        alert('no result');
+        return;
+    }
+
+    let dividir = numero1 / numero2;
+      document.getElementById('resultado4').textContent ='Resultado: ' + dividir;
+}
